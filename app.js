df332ab7c250473482b7d7bb5a55835aa46b84d2
tarari <t.jimenez@escolesnuria.cat>
2025-02-11 11:47:15 +0100

    Generalization

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors')
 const bodyParser = require('body-parser');
-const mysql = require('mysql');
+const mysql = require('mysql2');
 const dotenv = require('dotenv');
 dotenv.config();
 process.title="MyApp";
