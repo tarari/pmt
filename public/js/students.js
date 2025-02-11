3b09c3e435f6109af9819031d0d2027784a84632
tarari <t.jimenez@escolesnuria.cat>
2025-02-11 18:02:27 +0100

    Env generalization 3

--- public/js/students.js
+++ public/js/students.js
@@ -6,7 +6,7 @@ document.getElementById('addStudentForm').addEventListener('submit', async (e) =
     const student = {
       name: document.getElementById('name').value,
       email: document.getElementById('email').value,
-      group_name: document.getElementById('group').value,
+      group: document.getElementById('group').value,
     };
   const url=window.location.origin+":5001/students";
     try {
