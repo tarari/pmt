4a69fbbac6503943567da136404e1ae5c5399070
tarari <t.jimenez@escolesnuria.cat>
2025-02-20 15:49:36 +0100

    Canvis en main.js

--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,5 @@
-import * as dotenv from 'dotenv';
-process=dotenv.config();
-console.log(process.env.app.url);
-const url_env=process.env.app.url;
+
+const url_env=window.location.origin;
 async function fetchStudents() {
   const url=url_env+":5001/students";
   try {
